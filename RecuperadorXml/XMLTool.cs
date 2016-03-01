c948d1aedf056db007e4f139fe2f11ecde518cc0
Victor Mesquita <skovhm@gmail.com>
2016-03-01 11:02:53 -0300

    Mudanças no tratamento

--- RecuperadorXml/XMLTool.cs
+++ RecuperadorXml/XMLTool.cs
@@ -24,44 +24,62 @@ namespace RecuperadorXML
             string[] arquivos = Directory.GetFiles(temp);
             string xNome = "";
             string NF = "";
+            bool tratado = false;
 
             foreach (string xmls in arquivos)
             {
                 while (Path.GetExtension(xmls) == ".xml" && File.Exists(xmls))
                 {
-                    XmlDocument xmlDoc = new XmlDocument();
-                    xmlDoc.Load(xmls); //Carregando o arquivo
-
-                    XmlNodeList fornecedores = xmlDoc.GetElementsByTagName("emit");
-
-                    foreach (XmlNode fornecedor in fornecedores)
-                    {
-                        string nomeF = fornecedor["xNome"].InnerText;
-
-                        if (nomeF.Equals("Distribuidora de Medicamentos Santa Cruz Ltda Fl.16"))
+                        XmlDocument xmlDoc = new XmlDocument();
+                        try
                         {
-                            xNome = "santa";
+                            //Carregando o arquivo
+                            xmlDoc.Load(xmls);
+
                         }
-                        else
+                        catch (XmlException xmlE)
                         {
-                            xNome = nomeF.Substring(0, 6).ToLower();
+                            Console.WriteLine(xmlE.Message);
                         }
-                    }
 
-                    XmlNodeList notas = xmlDoc.GetElementsByTagName("ide");
-                    foreach (XmlNode nota in notas)
-                    {
-                        NF = nota["nNF"].InnerText;
-                    }
 
-                    File.Move(xmls, "C:\\asatransf\\" + xNome + "" + NF + ".xml");
-                    MessageBox.Show("Arquivos tratados com sucesso!", "Recuperador de XML", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                        XmlNodeList fornecedores = xmlDoc.GetElementsByTagName("emit");
+
+                        foreach (XmlNode fornecedor in fornecedores)
+                        {
+                            string nomeF = fornecedor["xNome"].InnerText;
+
+                            if (nomeF.Equals("Distribuidora de Medicamentos Santa Cruz Ltda Fl.16"))
+                            {
+                                xNome = "santa";
+                            }
+                            else
+                            {
+                                xNome = nomeF.Substring(0, 6).ToLower();
+                            }
+                        }
+
+                        XmlNodeList notas = xmlDoc.GetElementsByTagName("ide");
+                        foreach (XmlNode nota in notas)
+                        {
+                            NF = nota["nNF"].InnerText;
+                        }
+
+                        File.Move(xmls, "C:\\asatransf\\" + xNome.Trim() + "" + NF + ".xml");
+                        tratado = true;
                 }
             }
 
-            if ((Directory.GetFiles(temp) == null || Directory.GetFiles(temp).Length == 0))
+            if ((Directory.GetFiles(temp) == null || Directory.GetFiles(temp).Length <= 0) && tratado == false)
             {
                 MessageBox.Show("Nenhum arquivo para ser tratado", "Recuperador de XML", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (tratado)
+            {
+                MessageBox.Show("Arquivos tratados com sucesso!", "Recuperador de XML", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tratado = false;
             }
 
         }
